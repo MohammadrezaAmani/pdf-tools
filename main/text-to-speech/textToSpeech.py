8cd8412b1b337208767cb7e27ad40de58a6976d7
MohammadrezaAmani <more.amani@yahoo.com>
2022-02-12 01:01:59 +0330

    text to speech

--- main/text-to-speech/textToSpeech.py
+++ main/text-to-speech/textToSpeech.py
@@ -8,31 +8,41 @@
 import gtts
 import playsound
 
+
 class TextToSpeech:
-    def init(self, text, language = 'en'):
+    def init(self, text, language='en'):
         self.text = text
         self.language = language
         self.speech = None
         self.name = 'speech.mp3'
+
     def convert(self):
         self.speech = gtts.gTTS(self.text, self.language)
         print('Converted!')
+
     def save(self, path):
         self.speech.save(path)
         self.name = path
         print('Done!')
+
     def play(self):
         playsound.playsound(self.speech.save(self.name))
         print('Done!')
+
     def setText(self, text):
         self.text = text
+
     def getText(self):
         return self.text
+
     def setLanguage(self, language):
         self.language = language
+
     def getLanguage(self):
         return self.language
+
     def setName(self, name):
         self.name = name
+
     def getName(self):
         return self.name
